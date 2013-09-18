0c8e67283ca82039f2ccc42151213ec0dd20e49e
vagrant <vagrant@precise32.(none)>
2013-09-18 14:54:58 +0000

    initial commit

---
+++ kernel.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "hardware.h"
+#include "drivers.h"
+#include "kernel.h"
+
+
+
+/* You may use the definitions below, if you are so inclined, to
+   define the process table entries. Feel free to use your own
+   definitions, though. */
+
+
+typedef enum { RUNNING, READY, BLOCKED , UNINITIALIZED } PROCESS_STATE;
+
+typedef struct process_table_entry {
+  PROCESS_STATE state;
+  int CPU_time_used;
+  int quantum_start_time;
+} PROCESS_TABLE_ENTRY;
+
+extern PROCESS_TABLE_ENTRY process_table[];
+
+
+/* A quantum is 40 ms */
+
+#define QUANTUM 40
+
+PROCESS_TABLE_ENTRY process_table[MAX_NUMBER_OF_PROCESSES];
+
+
+/* You may use this code for maintaining a queue of
+   ready processes. It's simply a linked list structure
+   with each element containing the PID of a ready process */    
+
+typedef struct ready_queue_elt {
+  struct ready_queue_elt *next;
+  PID_type pid;
+} READY_QUEUE_ELT;
+
+
+READY_QUEUE_ELT *ready_queue_head = NULL;  /* head of the event queue */
+READY_QUEUE_ELT *ready_queue_tail = NULL;
+
+
+//places process id at end of ready queue
+
+void queue_ready_process(PID_type pid)
+{
+  //  printf("QUEUEING ready PID %d\n", pid);
+  READY_QUEUE_ELT *p = (READY_QUEUE_ELT *) malloc(sizeof(READY_QUEUE_ELT));
+  p->pid = pid;
+  p->next = NULL;
+  if (ready_queue_tail == NULL) 
+    if (ready_queue_head == NULL) {
+      ready_queue_head = ready_queue_tail = p;
+      p->next = NULL;
+    }
+    else {
+      printf("Error: ready queue tail is NULL but ready_queue_head is not\n");
+      exit(1);
+    }
+  else {
+    ready_queue_tail->next = p;
+    ready_queue_tail = p;
+  }
+}
+
+
+
+
+//removes and returns PID from front of ready queue
+
+PID_type dequeue_ready_process()
+{
+  if (ready_queue_head == NULL)
+    if (ready_queue_tail == NULL)
+      return IDLE_PROCESS;        // indicates no active process is ready
+    else {
+      printf("Error: ready_queue_head is NULL but ready_queue_tail is not\n");
+      exit(1);
+    }
+  else {      
+    READY_QUEUE_ELT *p = ready_queue_head;
+    ready_queue_head = ready_queue_head->next;
+    if (ready_queue_head == NULL)
+      ready_queue_tail = NULL;
+    return p->pid;
+  }
+}
+
+
+/* This procedure is automatically called when the 
+   (simulated) machine boots up */
+
+void initialize_kernel()
+{
+  // Put any initialization code you want here.
+  // Remember, the process 0 will automatically be
+  // executed after initialization (and current_pid
+  // will automatically be set to 0), 
+  // so the your process table should reflect that fact.
+}
+
---
+++ kernel.h
@@ -0,0 +1,7 @@
+
+/* This must be implemented by the kernel. It will
+   be called automatically during (simulated) system 
+   initialization */
+
+extern void initialize_kernel(); 
+
